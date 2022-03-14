286f8e6d757270bcb98cc8865f2535c6217cf4d4
Doiae9 <leonardogh75@gmail.com>
2022-03-14 11:36:03 -0600

    Receta terminada

--- src/Receta.java
+++ src/Receta.java
@@ -1,15 +1,26 @@
 import java.util.ArrayList;
 
 public class Receta {
+    private String nombre;
   private double tiempoPreparacion;
 private ArrayList<Ingrediente> ingredientes;
 private ArrayList<String> pasos;
 
-    public Receta(double tiempoPreparacion, ArrayList<Ingrediente> ingredientes, ArrayList<String> pasos) {
+    public Receta(String nombre, double tiempoPreparacion, ArrayList<Ingrediente> ingredientes, ArrayList<String> pasos) {
+        this.nombre = nombre;
         this.tiempoPreparacion = tiempoPreparacion;
         this.ingredientes = ingredientes;
         this.pasos = pasos;
         System.out.println("Receta creada");
+    }
+    public Receta(){}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public double getTiempoPreparacion() {
@@ -40,8 +51,18 @@ private ArrayList<String> pasos;
             System.out.println(ingrediente);
         }
         for (int i=0; i<ingredientes.size(); i++){
-            System.out.println(i+ "-"+ ingredientes.get(i));
+            System.out.println(i+1+ "-" + ingredientes.get(i));
         }
     }
-
+public void mostrarPasos(){
+        for (int i=0; i<pasos.size(); i++){
+            System.out.println(i+1 + ". "+pasos.get(i));
+        }
+}
+public void mostrarReceta(){
+    System.out.println(nombre);
+    System.out.println(tiempoPreparacion);
+    mostrarIngredientes();
+    mostrarPasos();
+}
 }
